f2f313c8c35c39da3baed87dd1ab82c1cd1d644f
sotownsend <so.townsend@gmail.com>
2013-05-24 12:38:43 -0400

    Improved igore

---
+++ Core/build/CMakeFiles/neurotik.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/neurotik.dir/main.cpp.o"
+  "CMakeFiles/neurotik.dir/Net.cpp.o"
+  "CMakeFiles/neurotik.dir/Schema.cpp.o"
+  "CMakeFiles/neurotik.dir/GetNet.cpp.o"
+  "CMakeFiles/neurotik.dir/Timer.cpp.o"
+  "CMakeFiles/neurotik.dir/CoreManager.cpp.o"
+  "neurotik.pdb"
+  "neurotik"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/neurotik.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
