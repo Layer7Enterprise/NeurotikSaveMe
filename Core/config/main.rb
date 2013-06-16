c4772d4a4107ac86e32dacd1ce0244d381f70649
sotownsend <so.townsend@gmail.com>
2013-06-16 09:41:39 -0400

    layer

--- Core/config/main.rb
+++ Core/config/main.rb
@@ -9,12 +9,15 @@ end
 
 main do
   no_learn "layer0", :count => 30
+  no_learn "layer1", :count => 30
 end
 
 #Arbitrary connections
 #connect "input", "layer0", :some_to_many, :count => 2, :weight => 12
 
 #Establish output
-connect "signal", "output", :linear, :delay => 6
+connect "signal", "output", :linear, :delay => 9
+
 connect "input", "layer0", :some_to_many, :count => 3, :weight => 12
-connect "layer0", "output", :many_to_many, :weight => 0
+connect "layer0", "layer1", :linear
+connect "layer1", "output", :many_to_many, :weight => 0
