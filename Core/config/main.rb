ce12df8836f8f4ae59cfdeaec7e73c6deed905bd
sotownsend <so.townsend@gmail.com>
2013-06-16 11:08:30 -0400

    trying to get it work better

--- Core/config/main.rb
+++ Core/config/main.rb
@@ -11,20 +11,20 @@ main do
   no_learn "layer0", :count => 100
   no_learn "layer1", :count => 100
   gaba "layer1i", 2, :count => 100
-  glu "layer2", :count => 200
+  glu "layer2", :count => 100
 end
 
-#Arbitrary connections
+nArbitrary connections
 #connect "input", "layer0", :some_to_many, :count => 2, :weight => 12
 
 #Establish output
-connect "signal", "output", :linear, :delay => 10
+connect "signal", "output", :linear, :delay => 11
 
 connect "input", "layer0", :some_to_many, :count => 3, :weight => 12
 connect "input", "layer0", :some_to_many, :count => 3, :weight => 12
 connect "layer0", "layer1", :linear
 connect "layer0", "layer1i", :linear
-connect "layer1", "layer2", :some_to_many, :count => 3, :weight => 12
+connect "layer1", "layer2", :all_to_some, :count => 3, :weight => 12
 connect "layer1i", "layer2", :some_to_many, :percent => 3
 
 connect "layer2", "output", :many_to_many, :weight => 0
