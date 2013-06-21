7ec8f440f079301b714dd3e77322b85a351735bc
sotownsend <so.townsend@gmail.com>
2013-06-21 09:09:46 -0400

    Trying to fix sort so that right connection order appears

--- Core/config/main.rb
+++ Core/config/main.rb
@@ -1,5 +1,5 @@
 input do 
-  glu "input", :count => 2
+ glu "input", :count => 2
   glu "signal", :debug => true, :count => 3
 end 
 
@@ -8,14 +8,11 @@ output do
 end
 
 main do
-  no_learn "inv", :count => 2, :debug => true
+ no_learn "inv", :count => 2, :debug => true
   gaba "invG", 5, :count => 2, :debug => true
-  #no_learn "layer1", :count => 100
-  #gaba "layer1i", 2, :count => 100
-  #glu "layer2", :count => 100
 end
 
-offset = 3
+offset = 0
 connect "signal", "output", :linear, :delay => 5 + offset
 connect "input", "output", :many_to_many, :weight => 0, :delay => 3 + offset
 
@@ -23,19 +20,3 @@ connect "input", "inv", :many_to_many, :delay => 4
 connect "invG", "inv", :linear
 connect "input", "invG", :linear
 connect "inv", "output", :many_to_many, :weight => 0
-#connect "layer0", "output", :many_to_many, :weight => 0
-
-##nArbitrary connections
-##connect "input", "layer0", :some_to_many, :count => 2, :weight => 12
-
-#Establish output
-
-#connect "input", "layer0", :some_to_many, :count => 2, :weight => 12
-#connect "input", "layer0", :some_to_many, :count => 2, :weight => 12
-#connect "layer0", "layer1", :linear
-#connect "layer0", "layer1i", :linear
-#connect "layer1", "layer2", :all_to_some, :count => 2, :weight => 12
-#connect "layer1i", "layer2", :some_to_many, :percent => 3
-
-#connect "layer2", "output", :many_to_many, :weight => 0
-
--- ncc/frame.rb
+++ ncc/frame.rb
@@ -2,23 +2,23 @@
 @is_currently_in_a_frame = false
 
 #Start a new frame (string)
-def begin_frame_string priority
+def begin_frame_string priority, sub_priority
   @current_frame = ""
 
   yield
 
-  add_compile_string priority, @current_frame
+  add_compile_string priority, sub_priority, @current_frame
 end
 
 #Start a frame (Code)
-def begin_frame priority, &block
+def begin_frame priority, sub_priority, &block
   proc = Proc.new do
-    begin_frame_string priority do 
+    begin_frame_string priority, sub_priority do 
       block.call
     end
   end
 
-  add_compile_block priority, proc
+  add_compile_block priority, sub_priority, proc
 end
 
 def write string
