4e96a09aeb4f2f106ef22c694cc6d3b2bc7d7caa
sotownsend <so.townsend@gmail.com>
2013-06-21 21:36:08 -0400

    ?

--- Core/config/main.rb
+++ Core/config/main.rb
@@ -1,39 +1,30 @@
 input do 
- glu "input", :count => 2
- glu_signal "signal", :debug => true, :count => 2
+  glu "input", :count => 10
+  glu "signal", :debug => true, :count => 3
 end 
 
 output do
-  glu "output", :debug => true, :count => 2
+  glu_signal "output", :debug => true, :count => 3
 end
 
 main do
-  no_learn "clock", :debug => true, :count => 1
-
-  gaba "invG", 5, :debug => true, :count => 2
-
-  no_learn "inv", :debug => true, :count => 2
-  no_learn "inputA", :debug => true, :count => 2
-
-  #Multi-layer
-  no_learn "layer0", :debug => true, :count => 20
+  no_learn "layer0", :count => 100
+  no_learn "layer1", :count => 100
+  gaba "layer1i", 2, :count => 100
+  glu "layer2", :count => 200
 end
 
-connect "input", "clock", :many_to_one
-connect "clock", "inv", :one_to_many
-connect "input", "invG", :linear
-connect "invG", "inv", :linear
+#Arbitrary connections
+#connect "input", "layer0", :some_to_many, :count => 2, :weight => 12
 
-#Align input
-connect "input", "inputA", :linear, :delay => 5
+#Establish output
+connect "signal", "output", :linear, :delay => 10
 
-#Connect to forest
-connect "inputA", "layer0", :some_to_many, :count => 1, :weight => 12
+connect "input", "layer0", :some_to_many, :count => 3, :weight => 12
+connect "input", "layer0", :some_to_many, :count => 3, :weight => 12
+connect "layer0", "layer1", :linear
+connect "layer0", "layer1i", :linear
+connect "layer1", "layer2", :some_to_many, :count => 3, :weight => 12
+connect "layer1i", "layer2", :some_to_many, :percent => 3
 
-connect "inv", "layer0", :some_to_many, :count => 1, :weight => 12
-
-#Connec to output
-#connect "inputA", "output", :many_to_many, :weight => 0
-#connect "inv", "output", :many_to_many, :weight => 0
-connect "layer0", "output", :many_to_many, :weight => 0
-connect "signal", "output", :linear, :delay => 13
+connect "layer2", "output", :many_to_many, :weight => 0
