a9e22a469f0f36efcf20f1633e93b8cc4e495834
sotownsend <so.townsend@gmail.com>
2013-06-26 10:33:10 -0400

    Wip

--- Core/config/main.rb
+++ Core/config/main.rb
@@ -1,30 +1,19 @@
 input do 
-  glu "input", :count => 10
-  glu "signal", :debug => true, :count => 3
+  glu "input", :debug => true, :count => 11
+  glu "signal", :debug => true, :count => 11
 end 
 
 output do
-  glu_signal "output", :debug => true, :count => 3
+  glu_signal "output", :debug => true, :count => 11
 end
 
 main do
-  no_learn "layer0", :count => 200
-  no_learn "layer1", :count => 200
-  gaba "layer1i", 2, :count => 200
-  glu "layer2", :count => 200
+  #no_learn "layer0", :count => 200
+  #no_learn "layer1", :count => 200
+  #gaba "layer1i", 2, :count => 200
+  #glu "layer2", :count => 200
 end
 
-#nArbitrary connections
-#connect "input", "layer0", :some_to_many, :count => 2, :weight => 12
-
-#Establish output
-connect "signal", "output", :linear, :delay => 11
-
-connect "input", "layer0", :some_to_many, :count => 3, :weight => 12
-connect "input", "layer0", :some_to_many, :count => 3, :weight => 12
-connect "layer0", "layer1", :linear
-connect "layer0", "layer1i", :linear
-connect "layer1", "layer2", :all_to_some, :count => 3, :weight => 12
-connect "layer1i", "layer2", :some_to_many, :percent => 3
-
-connect "layer2", "output", :many_to_many, :weight => 0
+connect "input", "input", :many_to_many, :weight => 0
+connect "signal", "input", :linear, :delay => 1
+connect "input", "output", :linear
