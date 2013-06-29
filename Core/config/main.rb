1bbca3d297dae0c36385b58d363b3e179bce9b06
sotownsend <so.townsend@gmail.com>
2013-06-28 23:27:36 -0400

    Before changing core to actively potentiating

--- Core/config/main.rb
+++ Core/config/main.rb
@@ -11,10 +11,10 @@ end
 main do
 end
 
-connect "input", "input", :many_to_many, :weight => 0
+connect "input", "input", :many_to_many, :weight => 0, :delay => 5
 #connect "input", "input", :many_to_many, :delay => 3
 #connect "input", "input", :many_to_many, :weight => 0, :delay => 8
 #connect "input", "input", :many_to_many, :weight => 0, :delay => 3
 
-connect "signal", "input", :linear, :delay => 3
+connect "signal", "input", :linear, :delay => 8
 connect "input", "output", :linear
